f81351ae557a5c320f95371bb4a4e96191c37fa4
Mahmoud Adel <onemahmoudadel@gamil.com>
2024-07-31 05:17:16 +0300

    fix addPost icons bg

--- components/icons.tsx
+++ components/icons.tsx
@@ -61,7 +61,7 @@ export const Icons = {
   ),
   addPost: (props: LucideProps) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <rect x="2" y="2" width="20" height="20" rx="4" fill="white" stroke="currentColor" strokeWidth="2"/>
+    <rect x="2" y="2" width="20" height="20" rx="4" fill="none" stroke="currentColor" strokeWidth="2"/>
     <path d="M7 12H17" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
     <path d="M12 7L12 17" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
   </svg>
